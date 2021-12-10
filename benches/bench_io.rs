c0044c8fb3b867d55ad7e001a206763c4999770e
Dev McElhinney <derwentx@gmail.com>
2021-12-10 18:10:19 +0800

    🎨 fmt benchmark

--- benches/bench_io.rs
+++ benches/bench_io.rs
@@ -7,10 +7,10 @@
 use criterion::*;
 use glob::glob;
 use marlu::{
+    io::{ms::MeasurementSetWriter, VisWritable},
     mwalib::CorrelatorContext,
-    ndarray::{Array3},
-    Jones, 
-    io::{ms::{MeasurementSetWriter}, VisWritable}, RADec, Complex
+    ndarray::Array3,
+    Complex, Jones, RADec,
 };
 use mwalib::get_baseline_count;
 use ndarray::Array4;
@@ -55,7 +55,8 @@ fn get_context_mwax_half_1247842824() -> CorrelatorContext {
 fn bench_ms_init_mwax_half_1247842824(crt: &mut Criterion) {
     let context = get_context_mwax_half_1247842824();
     let first_common_timestep_idx = *(context.common_timestep_indices.first().unwrap());
-    let mwalib_timestep_range = first_common_timestep_idx..(first_common_timestep_idx + NUM_TIMESTEPS);
+    let mwalib_timestep_range =
+        first_common_timestep_idx..(first_common_timestep_idx + NUM_TIMESTEPS);
     let mwalib_coarse_chan_range = 0..NUM_COARSE_CHANS;
 
     let tmp_dir = tempdir().unwrap();
@@ -64,45 +65,60 @@ fn bench_ms_init_mwax_half_1247842824(crt: &mut Criterion) {
     let phase_centre = RADec::from_mwalib_phase_or_pointing(&context.metafits_context);
     let ms_writer = MeasurementSetWriter::new(ms_path, phase_centre, None);
 
-    crt.bench_function("ms_writer::initialize_from_mwalib - mwax_half_1247842824", |bch| {
-        bch.iter(|| {
-            ms_writer
-                .initialize_from_mwalib(&context, &mwalib_timestep_range, &mwalib_coarse_chan_range)
-                .unwrap();
-        })
-    });
+    crt.bench_function(
+        "ms_writer::initialize_from_mwalib - mwax_half_1247842824",
+        |bch| {
+            bch.iter(|| {
+                ms_writer
+                    .initialize_from_mwalib(
+                        &context,
+                        &mwalib_timestep_range,
+                        &mwalib_coarse_chan_range,
+                    )
+                    .unwrap();
+            })
+        },
+    );
 }
 
-fn synthesize_test_data(shape: (usize, usize, usize)) -> (Array3<Jones<f32>>, Array4<f32>, Array4<bool>) {
-    let jones_array = Array3::from_shape_fn(
-        shape,
-        |(timestep_idx, chan_idx, baseline_idx)| {
-            Jones::from([
-                Complex::new(0., timestep_idx as _),
-                Complex::new(0., chan_idx as _),
-                Complex::new(0., baseline_idx as _),
-                Complex::new(0., 1.),
-            ])
-        }
-    );
+fn synthesize_test_data(
+    shape: (usize, usize, usize),
+) -> (Array3<Jones<f32>>, Array4<f32>, Array4<bool>) {
+    let jones_array = Array3::from_shape_fn(shape, |(timestep_idx, chan_idx, baseline_idx)| {
+        Jones::from([
+            Complex::new(0., timestep_idx as _),
+            Complex::new(0., chan_idx as _),
+            Complex::new(0., baseline_idx as _),
+            Complex::new(0., 1.),
+        ])
+    });
     let shape_with_pol = (shape.0, shape.1, shape.2, 4);
     let weight_array = Array4::from_shape_fn(
         shape_with_pol,
         |(timestep_idx, chan_idx, baseline_idx, pol_idx)| {
-            (timestep_idx * shape_with_pol.0 * shape_with_pol.1 * shape_with_pol.2 * shape_with_pol.3
+            (timestep_idx
+                * shape_with_pol.0
+                * shape_with_pol.1
+                * shape_with_pol.2
+                * shape_with_pol.3
                 + chan_idx * shape_with_pol.0 * shape_with_pol.1 * shape_with_pol.2
                 + baseline_idx * shape_with_pol.0 * shape_with_pol.1
                 + pol_idx) as f32
-        }
+        },
     );
     let flag_array = Array4::from_shape_fn(
         shape_with_pol,
         |(timestep_idx, chan_idx, baseline_idx, pol_idx)| {
-            (timestep_idx * shape_with_pol.0 * shape_with_pol.1 * shape_with_pol.2 * shape_with_pol.3
+            (timestep_idx
+                * shape_with_pol.0
+                * shape_with_pol.1
+                * shape_with_pol.2
+                * shape_with_pol.3
                 + chan_idx * shape_with_pol.0 * shape_with_pol.1 * shape_with_pol.2
                 + baseline_idx * shape_with_pol.0 * shape_with_pol.1
-                + pol_idx % 2 == 0) as _
-        }
+                + pol_idx % 2
+                == 0) as _
+        },
     );
     (jones_array, weight_array, flag_array)
 }
@@ -110,7 +126,8 @@ fn synthesize_test_data(shape: (usize, usize, usize)) -> (Array3<Jones<f32>>, Ar
 fn bench_ms_write_mwax_part_1247842824(crt: &mut Criterion) {
     let context = get_context_mwax_half_1247842824();
     let first_common_timestep_idx = *(context.common_timestep_indices.first().unwrap());
-    let mwalib_timestep_range = first_common_timestep_idx..(first_common_timestep_idx + NUM_TIMESTEPS);
+    let mwalib_timestep_range =
+        first_common_timestep_idx..(first_common_timestep_idx + NUM_TIMESTEPS);
     let mwalib_coarse_chan_range = 0..NUM_COARSE_CHANS;
 
     let tmp_dir = tempdir().unwrap();
@@ -123,7 +140,8 @@ fn bench_ms_write_mwax_part_1247842824(crt: &mut Criterion) {
         .initialize_from_mwalib(&context, &mwalib_timestep_range, &mwalib_coarse_chan_range)
         .unwrap();
 
-    let mwalib_baseline_idxs: Vec<usize> = (0..get_baseline_count(context.metafits_context.num_ants)).collect();
+    let mwalib_baseline_idxs: Vec<usize> =
+        (0..get_baseline_count(context.metafits_context.num_ants)).collect();
 
     let fine_chans_per_coarse = context.metafits_context.num_corr_fine_chans_per_coarse;
 
@@ -134,27 +152,30 @@ fn bench_ms_write_mwax_part_1247842824(crt: &mut Criterion) {
     );
     let (jones_array, weight_array, flag_array) = synthesize_test_data(shape);
 
-    crt.bench_function("ms_writer::write_vis_mwalib - mwax_half_1247842824", |bch| {
-        bch.iter(|| {
-            ms_writer
-            .write_vis_mwalib(
-                jones_array.view(),
-                weight_array.view(),
-                flag_array.view(),
-                &context,
-                &mwalib_timestep_range,
-                &mwalib_coarse_chan_range,
-                &mwalib_baseline_idxs,
-            )
-            .unwrap();
-        })
-    });
+    crt.bench_function(
+        "ms_writer::write_vis_mwalib - mwax_half_1247842824",
+        |bch| {
+            bch.iter(|| {
+                ms_writer
+                    .write_vis_mwalib(
+                        jones_array.view(),
+                        weight_array.view(),
+                        flag_array.view(),
+                        &context,
+                        &mwalib_timestep_range,
+                        &mwalib_coarse_chan_range,
+                        &mwalib_baseline_idxs,
+                    )
+                    .unwrap();
+            })
+        },
+    );
 }
 
 criterion_group!(
     name = io;
     config = Criterion::default().sample_size(NUM_ITERATIONS);
-    targets = 
+    targets =
         bench_ms_init_mwax_half_1247842824,
         bench_ms_write_mwax_part_1247842824
 );
