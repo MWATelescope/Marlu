65b8e8b9fd49ccf65e785ada94fee1c4e6801cd9
Christopher H. Jordan <christopherjordan87@gmail.com>
2022-05-21 14:57:22 +0800

    Fix files missing MPL-2.0 license text.
    Only pal.rs has a different license (LGPL-3.0).

--- src/io/ms.rs
+++ src/io/ms.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use crate::{
     average_chunk_f64, c32,
     io::error::{IOError, MeasurementSetWriteError::MeasurementSetFull},
