ba2c5a95f230fa812d965c975e47bd1a9e40b2d5
Christopher H. Jordan <christopherjordan87@gmail.com>
2022-06-23 13:12:49 +0800

    Make a new LmnRime type.
    The `LMN::prepare_for_rime` function previously returned a new LMN, but
    this was a bit misleading. This function now returns `LmnRime`, which
    is (hopefully) distinct and can be converted back to LMN.
    
    Also add `dot` functions on `LMN` and `LmnRime` to conveniently do a dot
    product with a `UVW`.
    
    Add tests for all aspects of LMN code.

--- src/pos/lmn.rs
+++ src/pos/lmn.rs
@@ -10,6 +10,8 @@
 
 use std::f64::consts::TAU;
 
+use super::uvw::UVW;
+
 /// (l,m,n) direction-cosine coordinates. There are no units (i.e.
 /// dimensionless).
 ///
@@ -28,15 +30,115 @@ pub struct LMN {
 }
 
 impl LMN {
+    /// Get the dot product of a [`UVW`] with a [`LMN`], i.e. `2 * pi * (u * l +
+    /// v * m + w * (n - 1))`
+    pub fn dot(self, uvw: UVW) -> f64 {
+        TAU * (uvw.u * self.l + uvw.v * self.m + uvw.w * (self.n - 1.0))
+    }
+
     /// Subtract 1 from `n` and multiply each of (`l`,`m`,`n`) by 2pi. This is
     /// convenient for application with the radio interferometer measurement
     /// equation (RIME), as performing some multiplies and subtracts ahead of
     /// time could result in many fewer FLOPs.
-    pub fn prepare_for_rime(self) -> LMN {
-        Self {
+    pub fn prepare_for_rime(self) -> LmnRime {
+        LmnRime {
             l: TAU * self.l,
             m: TAU * self.m,
             n: TAU * (self.n - 1.0),
         }
+    }
+}
+
+/// A "radio interferometer measurement equation (RIME)"-ready version of
+/// [`LMN`]; i.e. `LmnRime.l == 2 * pi * LMN.l`, `LmnRime.m == 2 * pi * LMN.m`,
+/// `LmnRime.n == 2 * pi * (LMN.n - 1)`.
+#[derive(Clone, Copy, Debug, Default)]
+pub struct LmnRime {
+    /// 2 * pi * l \[dimensionless\]
+    pub l: f64,
+    /// 2 * pi * m \[dimensionless\]
+    pub m: f64,
+    /// 2 * pi * (n - 1) \[dimensionless\]
+    pub n: f64,
+}
+
+impl LmnRime {
+    /// Get the dot product of a [`UVW`] with a [`LmnRime`], i.e. `2 * pi * (u *
+    /// l + v * m + w * (n - 1))`
+    pub fn dot(self, uvw: UVW) -> f64 {
+        uvw.u * self.l + uvw.v * self.m + uvw.w * self.n
+    }
+
+    /// Convert this [`LmnRime`] to a [`LMN`].
+    pub fn to_lmn(self) -> LMN {
+        LMN {
+            l: self.l / TAU,
+            m: self.m / TAU,
+            n: self.n / TAU + 1.0,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_abs_diff_eq;
+    use std::f64::consts::PI;
+
+    #[test]
+    fn test_lmn_dot() {
+        let lmn = LMN {
+            l: 0.5,
+            m: 0.5,
+            n: 0.707,
+        };
+        let uvw = UVW {
+            u: 1.0,
+            v: 2.0,
+            w: 3.0,
+        };
+        assert_abs_diff_eq!(lmn.dot(uvw), 3.9018580757585224);
+    }
+
+    #[test]
+    fn test_lmn_prepare_for_rime() {
+        let lmn = LMN {
+            l: 0.5,
+            m: 0.5,
+            n: 0.707,
+        };
+        let lmn_rime = lmn.prepare_for_rime();
+        assert_abs_diff_eq!(lmn_rime.l, PI);
+        assert_abs_diff_eq!(lmn_rime.m, PI);
+        assert_abs_diff_eq!(lmn_rime.n, -1.840973295003619);
+    }
+
+    #[test]
+    fn test_lmn_rime_dot() {
+        let lmn = LmnRime {
+            l: 0.5,
+            m: 0.5,
+            n: 0.707,
+        };
+        let uvw = UVW {
+            u: 1.0,
+            v: 2.0,
+            w: 3.0,
+        };
+        assert_abs_diff_eq!(lmn.dot(uvw), 3.621);
+    }
+
+    #[test]
+    fn test_lmn_rime_to_lmn() {
+        let lmn = LMN {
+            l: 0.5,
+            m: 0.5,
+            n: 0.707,
+        };
+        let lmn_rime = lmn.prepare_for_rime();
+        let lmn2 = lmn_rime.to_lmn();
+        assert_abs_diff_eq!(lmn.l, lmn2.l);
+        assert_abs_diff_eq!(lmn.m, lmn2.m);
+        assert_abs_diff_eq!(lmn.n, lmn2.n);
     }
 }
